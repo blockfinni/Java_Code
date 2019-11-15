5433f1ff5479319bbcb7f12d16596b90fcf79ffa
AJ <benfinnirr@gmail.com>
2019-11-15 12:03:49 -0800

    Wakatime and Github test

--- src/com/Blockfinni/Loopy.java
+++ src/com/Blockfinni/Loopy.java
@@ -6,6 +6,8 @@ class Loopy {
 
    public static void main(String[] args) {
     System.out.println("Successful Wakatime connection");
+      System.out.println("Wakatime Dashboard update");
+
   }
   
 }// end class
