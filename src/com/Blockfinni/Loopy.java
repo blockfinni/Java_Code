12436d4e31a481f006d4ba210547bdf9ea13d459
Blockfinni <aj.no.limit.1@gmail.com>
2019-11-15 12:08:48 -0800

    testing

--- src/com/Blockfinni/Loopy.java
+++ src/com/Blockfinni/Loopy.java
@@ -5,8 +5,11 @@ class Loopy {
   }
 
    public static void main(String[] args) {
-    System.out.println("Successful Wakatime connection");
+      System.out.println("Successful Wakatime connection");
       System.out.println("Wakatime Dashboard update");
+      String x = "Finally I can start coding endlessly";
+
+      System.out.println(x);
 
   }
   
