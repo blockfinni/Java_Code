8132ba15ab62489e6385a75e26aa0c15e86fc91c
Blockfinni <aj.no.limit.1@gmail.com>
2019-11-17 02:01:37 -0800

    Addition

--- src/com/Blockfinni/Loopy.java
+++ src/com/Blockfinni/Loopy.java
@@ -7,11 +7,19 @@ class Loopy {
    public static void main(String[] args) {
       System.out.println("Successful Wakatime connection");
       System.out.println("Wakatime Dashboard update");
-      String x = "Finally I can start coding endlessly";
+      String v = "Finally I can start coding endlessly";
+
+      String y = "1";
+      String a = "8";
+
+      int x = Integer.parseInt(y);
+      int z = Integer.parseInt(a);
+
+      double total = x + z;
+
+      System.out.println(total);
 
       
-
-      System.out.println(x);
 
   }
   
