2d5c5eef1ec60c6f5ae033e88a52a3dd64d63c8b
Blockfinni <aj.no.limit.1@gmail.com>
2019-11-17 23:27:07 -0800

    Structure and Selection code

---
+++ src/com/Blockfinni/Selections.java
@@ -0,0 +1,25 @@
+/**************************************************
+*
+*   Description: All about selections in this one
+*
+*   Author: AJ
+**************************************************/
+
+
+
+class Selections{
+
+     public static void main(String[] args) {
+
+        String x = "Xray";
+
+        if (x.length() > 0){
+            System.out.println(" The string contains an alphabet");
+        }else{
+            System.out.println("Sorry no alphabets");
+        }
+        
+    }// end main
+
+    
+} // end class selections
