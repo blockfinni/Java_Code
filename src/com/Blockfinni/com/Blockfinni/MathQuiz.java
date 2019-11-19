64585f25ee112c34a6a8dc1c2dc441791e116c36
Blockfinni <aj.no.limit.1@gmail.com>
2019-11-18 22:48:49 -0800

    Creating a Math Quiz for kids

---
+++ src/com/Blockfinni/com/Blockfinni/MathQuiz.java
@@ -0,0 +1,65 @@
+package com.Blockfinni;
+
+import java.util.Scanner;
+
+
+
+class MathQuiz {
+
+
+    public static Scanner sc = new Scanner(System.in);
+
+
+    public static class Addition {
+
+        //attributes
+        private double number1;
+        private double number2;
+
+        Addition(double number1, double number2){
+
+            //variables intialized
+            this.number1 = number1;
+            this.number2 = number2;
+
+
+        }// end constructor
+
+        // Getters and Setters
+        public double getNumber1() {
+            return this.number1;
+        }
+
+        public void setNumber1(double number1) {
+            this.number1 = number1;
+        }
+
+        public double getNumber2() {
+            return this.number2;
+        }
+
+        public void setNumber2(double number2) {
+            this.number2 = number2;
+        }
+
+    }//end class Addition
+
+    public static void main(String[] args) {
+
+
+        double ranNum1 = Math.random() * 10;
+        double ranNum2 = Math.random() * 10;
+        double input1;
+        
+
+        
+
+        
+
+        
+        
+        
+    }
+
+
+}// end class
